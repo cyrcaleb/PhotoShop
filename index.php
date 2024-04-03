77ea3327fa1c94079a2a3e48ee4bda3c072e6de0
sarmenti7 <84134786+sarmenti7@users.noreply.github.com>
2024-04-03 08:45:58 -0400

    Update index.php

--- index.php
+++ index.php
@@ -15,11 +15,12 @@
 		// SQL query to retrieve toy information based on the toy ID
 		$sql = "SELECT * 
 			FROM toy
-			WHERE toynum= :id;";	// :id is a placeholder for value provided later 								It's a parameterized query that helps prevent SQL 								injection attacks and ensures safer interaction with 						 	 the database.
+			WHERE toynum= :id;";	// :id is a placeholder for value provided later 
+		                               // It's a parameterized query that helps prevent SQL injection attacks and ensures safer interaction with the database.
 
 
 		// Execute the SQL query using the pdo function and fetch the result
-		$toy = pdo($pdo, $sql, ['id' => $id])->fetch();		// Associative array where 'id' 													is the key and $id is the value. 													 Used to bind the value of $id to 													  the placeholder :id in  SQL query.
+		$toy = pdo($pdo, $sql, ['id' => $id])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
 
 		// Return the toy information (associative array)
 		return $toy;
