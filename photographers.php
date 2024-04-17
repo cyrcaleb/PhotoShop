8964893f28d8a937300cc54b087e9315f94539ce
Connor Montague <connor_montague@uri.edu>
2024-04-17 11:44:11 -0400

    photographer id fetching via loop

--- photographers.php
+++ photographers.php
@@ -5,7 +5,7 @@
 
     function get_photographer(PDO $pdo, string $id) {
 
-		// SQL query to retrieve toy information based on the toy ID
+		// SQL query to retrieve photographer information based on the toy ID
 		$sql = "SELECT * 
 			FROM Photographer
 			WHERE photographerID = :id;";	// :id is a placeholder for value provided later 
@@ -15,8 +15,18 @@
 		// Execute the SQL query using the pdo function and fetch the result
 		$photographer = pdo($pdo, $sql, ['id' => $id])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
 
-		// Return the toy information (associative array)
+		// Return the photographer information (associative array)
 		return $photographer;
+	}
+	
+	// Retrieve info for ALL photographers from the db
+	$photographers = []; // Initialize an array to store info for all photographers
+
+	// Fetch data for each remaining photographer using a loop
+	for ($i = 1; $i < 6; $i++) {
+
+	    $photographer_id = '3' . sprintf('%10d', $i); // Format the photograher ID with leading zeros
+	    $photographers[] = get_toy($pdo, $photographer_id); // Fetch photographer info and add to the array
 	}
 
 ?>
