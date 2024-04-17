fd497a27f6d0f341405ccfc4e973c946fb59fe61
Rafael Mendez <rmendez@8.3.20.172.s.wireless.uri.edu>
2024-04-17 11:41:33 -0400

    Added login.php

--- startScreen.php
+++ startScreen.php
@@ -13,20 +13,3 @@
     </form>
 </body>
 </html>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['user_type']) && ($_POST['user_type'] == "Photographer" || $_POST['user_type'] == "Customer")) {
-        $user_type = $_POST['user_type'];
-        if ($user_type == "Photographer") {
-            header("Location: photographer_login.php");
-            exit;
-        } elseif ($user_type == "Customer") {
-            header("Location: customer_login.php");
-            exit;
-        }
-    } else {
-        echo "Invalid user type.";
-    }
-}
-?>
