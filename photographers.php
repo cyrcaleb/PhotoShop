f1520b75b5c0fec815f521e2fd38645da92ded93
Connor Montague <connor_montague@uri.edu>
2024-04-18 15:39:48 -0400

    finalized name changes so they dont suck

--- photographers.php
+++ photographers.php
@@ -38,7 +38,7 @@
 		<meta charset="UTF-8">
   		<meta name="viewport" content="width=device-width, initial-scale=1.0">
   		<title>PhotoShop</title>
-  		<link rel="stylesheet" href="css/style1.css">
+  		<link rel="stylesheet" href="css/photostyle.css">
   		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
