3d20927d04436f80bcafa6b0ebae4ee72837e821
Caleb <caleb_cyr@uri.edu>
2024-04-21 16:55:12 -0400

    photographer.php photographer view

--- photographer.php
+++ photographer.php
@@ -83,7 +83,9 @@
 
 		    <div class="header-right">
 		    	<ul>
-		    		<li><a href="p_order.php">Check Order</a></li>
+					<li><a href="p_order.php">Check Order</a></li>
+					<li><a href="upload.php">Upload Photos</a></li>
+					<li><a href="newShoot.php">New Photoshoot</a></li>
 		    	</ul>
 		    </div>
 		</header>
