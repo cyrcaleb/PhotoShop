8aaf86edda926df9f09cc77efd888dade1b8983a
Rafael Mendez <rmendez@rafaels-mbp.mynetworksettings.com>
2024-04-21 17:25:05 -0400

    Fixed login to not require @

--- login2.php
+++ login2.php
@@ -20,8 +20,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {     // If form submitted
 <?php include 'includes/header-member.php'; ?>
 <h1>Login</h1>
 <form method="POST" action="login.php">
-  Email: <input type="email" name="email"><br>
-  Password: <input type="password" name="password"><br>
+  Email: <inpuT><br>
+  Password: <input><br>
   <input type="submit" value="Log In">
 </form>
 <?php include 'includes/footer.php'; ?>
