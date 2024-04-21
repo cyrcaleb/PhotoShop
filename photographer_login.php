a8ef53e07092fad67dbe00b46205fe2c5f883a45
Rafael Mendez <rmendez@rafaels-mbp.mynetworksettings.com>
2024-04-21 14:14:10 -0400

    Testing

--- photographer_login.php
+++ photographer_login.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Photographer Place Holder</title>
+    <title>Photographer Place Holder Test again</title>
 </head>
+<body>
+    
+</body>
 </html>
