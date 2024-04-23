3df7b298748a7b927c47cbab66989c4db6cbcb0a
benchau <ben842002@uri.edu>
2024-04-22 20:44:34 -0400

    add initial logic to display images

--- photos.php
+++ photos.php
@@ -8,12 +8,14 @@
 	 * @param int $numPhotos    The number of photos to retrieve.
 	 */
     function RetrieveRandomPhotos($pdo, int $numPhotos) {
-        $sql = "SELECT * FROM photos ORDER BY RAND() LIMIT :numPhotos;";
+        $sql = "SELECT * FROM Photo ORDER BY RAND() LIMIT :numPhotos;";
         $stmt = $pdo->prepare($sql);
         $stmt->execute(['numPhotos' => $numPhotos]);
 
         return $stmt->fetchAll();
     }
+
+	$randomPhotos = RetrieveRandomPhotos($pdo, 15);
 ?>
 
 <!DOCTYPE>
@@ -22,13 +24,49 @@
 		<meta charset="UTF-8">
   		<meta name="viewport" content="width=device-width, initial-scale=1.0">
   		<title>PhotoShop</title>
-  		<link rel="stylesheet" href="css/style.css">
+  		<link rel="stylesheet" href="css/photostyle.css">
   		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
 	</head>
 
     <body>
-    
+		<header>
+			<div class="header-left">
+				<div class="logo">
+					<img src="imgs/PhotoShopLogo.png" alt="PhotoShop Logo">
+      			</div>
+
+	      		<nav>
+	      			<ul>
+	      				<li><a href="photographer_catalog.php">Photographers</a></li>
+	      				<li><a href="about.php">About</a></li>
+			        </ul>
+			    </nav>
+		   	</div>
+
+		    <div class="header-right">
+		    	<ul>
+					<li><a href="p_order.php">Check Order</a></li>
+					<!-- <li><a href="upload.php">Upload Photos</a></li>
+					<li><a href="newShoot.php">New Photoshoot</a></li> -->
+		    	</ul>
+		    </div>
+		</header>
+
+		<?php
+			foreach ($randomPhotos as $photo) {
+		?>
+		<div class="toy-card">
+			<!-- TODO: Link to correct photographer page -->
+			<a href="toy.php?toynum=<?= $photo['toynum'] ?>">
+				<img src="<?= $photo['imgSrc'] ?>" alt="<?= $photo['name'] ?>">
+			</a>
+			<h2><?= $photo['name'] ?></h2>
+			<p>$<?= $photo['price'] ?></p>
+		</div>
+		<?php
+			}
+		?>
     </body>
 </html>
