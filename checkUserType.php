b2f4493350fdba491a9e47940f4af9935a3dd387
Rafael Mendez <rmendez@rafaels-mbp.myfiosgateway.com>
2024-04-23 11:54:19 -0400

    Fixes

--- checkUserType.php
+++ checkUserType.php
@@ -6,14 +6,16 @@ error_reporting(E_ALL); ini_set('display_errors', 1);
 // Include the database connection script
 require 'includes/database-connection.php';
 
-// Assume you have a session started already
-
 // Check if the user is logged in
 if (isset($_SESSION['user_type']) && $_SESSION['user_type'] === 'Photographer') {
+    //Print "WE MADE IT IN"
+    echo "WE MADE IT IN";
+
     // User is logged in as a photographer
     $isPhotographerLoggedIn = true;
 } else {
     // User is not logged in as a photographer
+    echo "WE MADE IT OUT";
     $isPhotographerLoggedIn = false;
 }
 ?>
